e64757cc5cd70a342a337139b6b8b7a091d26ea1
Daniel Huigens <d.huigens@gmail.com>
2017-10-01 03:58:16 +0200

    Fullscreen presentation and not Airborn OS on F11 and Ctrl+Cmd+F

--- app/preview_export/scripts/loadPresentation.js
+++ app/preview_export/scripts/loadPresentation.js
@@ -7,3 +7,24 @@ var loadPresentation = function() {
 	//	document.body.className = config.surface + " " + document.body.className;
 	}
 };
+
+/* Airborn OS: fullscreen presentation and not Airborn OS on F11 and Ctrl+Cmd+F */
+document.addEventListener('keydown', function(event) {
+	if(event.keyCode === 122 || (event.ctrlKey && event.metaKey && event.keyCode === 70)) {
+		if(window.fullScreen || document.webkitCurrentFullScreenElement) {
+			document.exitFullScreen();
+		} else {
+			document.documentElement.requestFullScreen();
+			document.documentElement.style.width = '100%'; // Webkit
+		}
+		event.preventDefault();
+	}
+});
+Element.prototype.requestFullScreen =
+	Element.prototype.requestFullScreen ||
+	Element.prototype.mozRequestFullScreen ||
+	Element.prototype.webkitRequestFullScreen;
+document.exitFullScreen =
+	document.exitFullScreen ||
+	document.mozCancelFullScreen ||
+	document.webkitExitFullscreen;
